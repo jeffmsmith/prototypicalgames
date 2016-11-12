9c2e85d1e545da4b69f4cf384b2ec5d6e78d51f7
Jeff Smith <jeff.smith@shiftwise.com>
2016-11-11 17:40:31 -0800

    fixed more formatting

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,5 @@
 angular.module('app')
 .config(function($mdThemingProvider) {
-    var background = $mdThemingProvider.extendPalette('grey', {
-      'A100': 'f2f2f2'
-    });
-    $mdThemingProvider.definePalette('background', background);
- 
   $mdThemingProvider.theme('default')
     .primaryPalette('light-blue')
-    .accentPalette('brown')
-    .backgroundPalette('background')
 });
